70b00c2cf79162dcbdbea0613c34b2817bb8c6b5
josephsanglee <josephsanglee@gmail.com>
2016-10-06 15:56:09 -0700

    Create crontab script

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,12 +1,15 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
-var archive = require('../helpers/archive-helpers');
+var archive = require('/Users/student/Desktop/2016-09-web-historian/helpers/archive-helpers.js');
 var fs = require('fs');
 var path = require('path');
 var http = require('http');
 
 exports.fetch = function() {
+  console.log('we fetching');
   archive.readListOfUrls(function(urlsArray) {
     archive.downloadUrls(urlsArray.slice(0, urlsArray.length - 1));
   });
 };
+
+exports.fetch();
