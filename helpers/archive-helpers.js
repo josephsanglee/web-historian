d98e16b5471dbb6d4dd020ee7018fa8eca7d1fb0
josephsanglee <josephsanglee@gmail.com>
2016-10-06 19:32:52 -0700

    Attempt re-factoring of archive-helpers to promises

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -3,6 +3,7 @@ var path = require('path');
 var _ = require('underscore');
 var http = require('http');
 var https = require('https');
+var Promise = require('bluebird');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -40,11 +41,32 @@ exports.readListOfUrls = function(cb) {
 
 };
 
+// exports.readListOfUrls = function() {
+//   return new Promise(function(reject, resolve) {
+//     fs.readFile(exports.paths.list, 'utf8', function(err, data) {
+//       if (err) { return reject(err, null); }
+//       var inputArray = data.split('\n');
+//       console.log(inputArray);
+      
+//       return resolve(null, inputArray);
+//     });
+//   });
+// };
+
 exports.isUrlInList = function(target, cb) {
   exports.readListOfUrls(function(urlsArray) {
     cb(urlsArray.indexOf(target) !== -1);
   });
 };
+
+// exports.isUrlInList = function(target) {
+//   return new Promise(function(reject, resolve) {
+//     exports.readListOfUrls()
+//     .then(function(inputArray) {
+//       resolve(urlsArray.indexOf(target) !== -1);
+//     });
+//   });
+// };
 
 exports.addUrlToList = function(url, cb) {
   exports.isUrlInList(url, function(exists) {
@@ -56,12 +78,33 @@ exports.addUrlToList = function(url, cb) {
   });
 };
 
+// exports.addUrlToList = function(url) {
+//   return new Promise(function(reject, resolve) {
+//     exports.isUrlInList(url)
+//     .then(function(exists) {
+//       if (exists) { return reject(); }
+
+//       fs.appendFile(exports.paths.list, url + '\n', 'utf8', resolve);
+//     });
+//   });
+// };
+
 exports.isUrlArchived = function(target, cb) {
   fs.readdir(exports.paths.archivedSites, function(err, files) {
     if (err) { return console.log('err'); }
     cb(files.indexOf(target) !== -1);
   });
 };
+
+// exports.isUrlArchived = function(target) {
+//   return new Promise(function(reject, resolve) {
+//     fs.readdir(exports.paths.archivedSites, function(err, files) {
+//       if (err) { return reject(err, null); }
+
+//       resolve(null, files.indexOf(target) !== -1);
+//     });
+//   });
+// };
 
 exports.downloadUrls = function(urlsArray) {
   urlsArray.forEach(function(url) {
@@ -86,3 +129,28 @@ exports.downloadUrls = function(urlsArray) {
     });
   });
 };
+
+// exports.downloadUrls = function(urlsArray) {
+//   urlsArray.forEach(function(url) {
+//     exports.isUrlArchived(url)
+//     .then(function(exists) {
+//       if (exists) { return; }
+//       var body = '';
+
+//       var request = http.get({'host': url}, function(response) {
+//         response.on('data', function(data) {
+//           body += data;
+//         });
+//         response.on('end', function() {
+//           var filePath = exports.paths.archivedSites + '/' + url;
+//           fs.writeFile(filePath, body.toString(), function(err) {
+//             if (err) { return console.log(err); }
+
+//             console.log('downloaded!');
+//           });
+//         });
+//       });
+//       request.end();
+//     });
+//   });
+// };
--- web/request-handler.js
+++ web/request-handler.js
@@ -2,6 +2,7 @@ var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 var httpHelpers = require('./http-helpers');
+var Promise = require('bluebird');
 
 exports.handleRequest = function (req, res) {
   var url = req.url;
@@ -51,7 +52,33 @@ exports.handleRequest = function (req, res) {
           });
         }
       });
-
     });
+
+    // req.on('data', function(data) {
+    //   // console.log('here first');
+    //   data = '' + data;
+    //   var url = data.slice(4);
+
+    //   archive.isUrlArchived(url)
+    //   .then(function(exists) {
+    //     if (exists) {
+          
+    //       res.writeHead(302, {Location: 'http://127.0.0.1:8080/' + url});
+    //       res.end();
+    //     } else {
+    //       archive.addUrlToList(url.toString())
+    //       .then(function() {
+    //         // console.log('here');
+    //         fs.readFile(archive.paths.loading, 'utf8', function(err, data) {
+    //           if (err) { return console.log(err); }
+
+    //           res.writeHead(302, httpHelpers.headers);
+    //           res.write(data);
+    //           res.end();
+    //         });
+    //       });
+    //     }
+    //   });
+    // });
   }
 };
